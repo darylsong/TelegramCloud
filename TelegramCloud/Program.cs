72f2acc2a82e966690e7bf75d30f0ee76831448f
Daryl Song <songlwdaryl@gmail.com>
2025-01-18 21:33:49 +0800

    add exception handler

--- TelegramCloud/Program.cs
+++ TelegramCloud/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.CommandLine;
+using System.CommandLine.Builder;
 using TelegramCloud.Commands.Configurations;
 using TelegramCloud.Commands.File;
 
@@ -6,5 +7,12 @@ var rootCommand = new RootCommand();
 
 rootCommand.AddCommand(new ConfigurationCommand());
 rootCommand.AddCommand(new FileCommand());
+
+new CommandLineBuilder(rootCommand)
+    .UseExceptionHandler((exception, _) =>
+    {
+        Console.WriteLine(exception.Message);
+    }, errorExitCode: 1)
+    .Build();
 
 await rootCommand.InvokeAsync(args);

6b9f9b7ef5466af79acc56ca870c31f201a2ba05
Daryl Song <daryl.song@titansoft.com.sg>
2025-01-19 20:57:23 +0800

    use primary constructors

--- TelegramCloud/Commands/File/Shared/FileIdArgument.cs
+++ TelegramCloud/Commands/File/Shared/FileIdArgument.cs
@@ -2,12 +2,5 @@ using System.CommandLine;
 
 namespace TelegramCloud.Commands.File.Shared;
 
-public class FileIdArgument : Argument<Guid>
-{
-    public FileIdArgument() : base(
-        name: "fileId",
-        description: "File ID"
-    )
-    {
-    }
-}
+public class FileIdArgument() : Argument<Guid>(name: "fileId",
+    description: "File ID");
--- TelegramCloud/Commands/File/Upload/FilePathArgument.cs
+++ TelegramCloud/Commands/File/Upload/FilePathArgument.cs
@@ -2,12 +2,5 @@ using System.CommandLine;
 
 namespace TelegramCloud.Commands.File.Upload;
 
-public class FilePathArgument : Argument<string>
-{
-    public FilePathArgument() : base(
-        name: "filePath",
-        description: "Path to file to be uploaded"
-    )
-    {
-    }
-}
+public class FilePathArgument() : Argument<string>(name: "filePath",
+    description: "Path to file to be uploaded");
